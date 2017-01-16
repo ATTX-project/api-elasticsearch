31fd760dd71aa940175187982424dd40723015dd
Stefan Negru <stefan.negru@helsinki.fi>
2017-01-16 17:08:31 +0200

    ATTX-project/project-management/issues#68 fixing BDD tests

--- dc-elasticsearch-siren/attx-api-plugin-feature-tests/src/test/java/org/uh/attx/bdd/api/test/StepDefinitions.java
+++ dc-elasticsearch-siren/attx-api-plugin-feature-tests/src/test/java/org/uh/attx/bdd/api/test/StepDefinitions.java
@@ -8,102 +8,93 @@ package org.uh.attx.bdd.api.test;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
+
 import com.mashape.unirest.request.GetRequest;
-import cucumber.api.PendingException;
-import cucumber.api.java.Before;
-import cucumber.api.java.en.Given;
-import cucumber.api.java8.En;
-import java.io.File;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import junit.framework.Assert;
+
+import cucumber.api.java8.En;
 import junit.framework.TestCase;
 import net.javacrumbs.jsonunit.JsonAssert;
-import net.javacrumbs.jsonunit.fluent.JsonFluentAssert;
-import org.json.JSONException;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.Assert;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
- *
  * @author jkesanie
  */
 public class StepDefinitions implements En {
+    private List<String> where = new ArrayList<String>();
 
     public StepDefinitions() throws Exception {
 
-        
-        Given("^runtime environment is in place$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            //throw new PendingException();
-        });
 
-        Given("^component image is available$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            //throw new PendingException();
-        });
-
-        When("^component is finished with startup$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            //throw new PendingException();
-        });
-
-        Then("^components API should be accessible via HTTP$", () -> {
+        Given("^component is running$", () -> {
             try {
                 GetRequest get = Unirest.get("http://localhost:9200").header("accept", "application/json");
-                HttpResponse<JsonNode> response =  get.asJson();
+                HttpResponse<JsonNode> response = get.asJson();
                 JSONObject result = response.getBody().getObject();
                 JsonAssert.assertJsonPartEquals(200, result, "status");
                 JsonAssert.assertJsonPartEquals("1.3.4", result, "version.number");
-                
+
             } catch (Exception ex) {
                 Logger.getLogger(StepDefinitions.class.getName()).log(Level.SEVERE, null, ex);
                 TestCase.fail(ex.getMessage());
-            } 
+            }
         });
 
-        Given("^component is running$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            //throw new PendingException();
-        });
-
-        When("^component is sent a stop signal$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
-        });
-
-        Then("^component's api should not available anymore$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            throw new PendingException();
-        });
 
         When("^listing available plugins$", () -> {
-            // Write code here that turns the phrase above into concrete actions
-            //throw new PendingException();
-        });
-
-        Then("^all the required plugins should be installed succesfully$", () -> {
             try {
                 GetRequest get = Unirest.get("http://localhost:9200/_nodes/").header("accept", "application/json");
-                HttpResponse<JsonNode> response =  get.asJson();
-                
+                HttpResponse<JsonNode> response = get.asJson();
+
                 JSONObject nodes = response.getBody().getObject().getJSONObject("nodes");
                 Iterator<String> keys = nodes.keys();
                 String key = keys.next(); // first node
-                JSONObject result = nodes.getJSONObject(key);
-                
-                JsonAssert.assertJsonPartEquals("analysis-icu", result, "plugins[0].name");
-                JsonAssert.assertJsonPartEquals("siren-plugin", result, "plugins[1].name");
-                
-                
+                JSONObject thekeys = nodes.getJSONObject(key);
+                JSONArray plugins = (JSONArray) thekeys.get("plugins");
+                Iterator i = plugins.iterator();
+
+                while (i.hasNext()) {
+                    JSONObject plugin = (JSONObject) i.next();
+                    String name = (String) plugin.get("name");
+                    where.add(name);
+                }
+
+                assertEquals(response.getStatus(), 200);
+//                JsonAssert.assertJsonPartEquals("analysis-icu", result, "plugins[0].name");
+//                JsonAssert.assertJsonPartEquals("siren-plugin", result, "plugins[1].name");
+
+
             } catch (Exception ex) {
                 Logger.getLogger(StepDefinitions.class.getName()).log(Level.SEVERE, null, ex);
                 TestCase.fail(ex.getMessage());
-            }            
+            }
+        });
+
+        Then("^all the required plugins should be installed successfully\\.$", () -> {
+            try {
+                assertTrue(where.contains("ATTXApiPlugin"));
+                assertTrue(where.contains("siren-plugin"));
+                assertTrue(where.contains("analysis-icu"));
+
+            } catch (Exception ex) {
+                Logger.getLogger(StepDefinitions.class.getName()).log(Level.SEVERE, null, ex);
+                TestCase.fail(ex.getMessage());
+            }
+
         });
     }
 }
